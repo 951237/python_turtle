eb1447f3733ed009c02c2ebf46355b52982c2f5c
951237 <951237.gmail.com>
2020-02-02 23:57:22 +0900

    Doc : Update - 변수이름 변경

--- draw_line_360_200202.py
+++ draw_line_360_200202.py
@@ -1,14 +1,15 @@
 # 선으로 원그리
 
-import turtle, time
+import time
+import turtle
 
 ws = turtle.Screen()
 t = turtle.Turtle()
 t.speed(0)
 t.shape(name='turtle')
 
-v_wt_line = 10  # 원하는 라인의 갯수
-v_wt_angle = 360 / v_wt_line  # 원하는 라인을 360도로 그리는데 필요한 각 계
+v_want_line = 10  # 원하는 라인의 갯수
+v_want_angle = 360 / v_want_line  # 원하는 라인을 360도로 그리는데 필요한 각 계
 
 
 def d_line(v_line, v_angle):
@@ -18,6 +19,7 @@ def d_line(v_line, v_angle):
         t.forward(200)
         t.left(180 + v_angle)  # 10도 간격으로 그리고 싶으면 180 + 10 = 190
 
-d_line(v_wt_line, v_wt_angle)
+
+d_line(v_want_line, v_want_angle)
 
 time.sleep(5)
